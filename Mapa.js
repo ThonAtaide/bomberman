66ab80be78a5e339e0580ca8dc36fa72a4968d21
ThonAtaide <thon_ataide10@hotmail.com>
2018-09-10 21:33:48 -0300

    Mapa and mapaMascara update

---
+++ Mapa.js
@@ -0,0 +1,52 @@
+function Mapa(){
+
+}
+//Compreensão do conteúdo do mapa 
+Mapa.LIVRE = 0;
+Mapa.PAREDE = 1;
+Mapa.JOGADOR = 2;
+Mapa.TIJOLO = 3;
+Mapa.INIMIGO = 4;
+Mapa.BOMBA = 5;
+
+//linhas e colunas existentes no mapa atualmente
+
+Mapa.colunas = 0;
+Mapa.linhas = 0;
+
+//Largura e altura de cada linha/coluna em pixels 
+Mapa.largura = 40;
+
+Mapa.atual = null;
+
+Mapa.carregar = function (canvas) { 
+    Mapa.atual = new Array(); 
+    var y, x; 
+    for (y = 0; y < mapaMascara.length; y++) { 
+        Mapa.atual.push(mapaMascara[y].slice(0));
+    }
+    Mapa.linhas = Mapa.atual.length;
+    Mapa.colunas = Mapa.atual[0].length; 
+    canvas.width = Mapa.colunas * Mapa.largura;
+    canvas.height = Mapa.linhas * Mapa.largura; 
+};
+
+Mapa.imgTijolo = new Image(); 
+Mapa.imgTijolo.src = "tijolo.png"; 
+
+Mapa.desenhar = function (ctx) { 
+    if (Mapa.atual != null) { 
+        var x, y; 
+        for (y = 0; y < Mapa.linhas; y++) { 
+            for (x = 0; x < Mapa.colunas; x++) { 
+                if (Mapa.atual[y][x] == Mapa.PAREDE) { 
+                    ctx.fillStyle = "#BBBBBB"; 
+                    ctx.fillRect(x * Mapa.largura, y * Mapa.largura, Mapa.largura, Mapa.largura); 
+                } else 
+                    if (Mapa.atual[y][x] ==  Mapa.TIJOLO) { 
+                        ctx.drawImage(Mapa.imgTijolo, x * Mapa.largura, y * Mapa.largura, Mapa.largura, Mapa.largura); 
+                    } 
+            }
+        }
+    }
+};
